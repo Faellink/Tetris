77e04fa894c367d8fefbcd1026e9fda4a37c7236
Rafael Araújo <rafaelgdv0@gmail.com>
2022-06-06 16:08:31 -0300

    board grid

--- Tetris/Tetris/Tetris.cs
+++ Tetris/Tetris/Tetris.cs
@@ -12,11 +12,16 @@ namespace Tetris
 {
     public partial class TetrisForm : Form
     {
+
+        
+
         public TetrisForm()
         {
             InitializeComponent();
+
+            
         }
 
-
+        
     }
 }
