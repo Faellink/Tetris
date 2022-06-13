591072494a6627999e52a12c0f0847f6d1ff6f89
Rafael Araújo <rafaelgdv0@gmail.com>
2022-06-13 15:17:05 -0300

    int grid

--- Tetris/Tetris/Tetris.cs
+++ Tetris/Tetris/Tetris.cs
@@ -13,11 +13,13 @@ namespace Tetris
     public partial class TetrisForm : Form
     {
 
-        public Block[,] grid = new Block[20, 10];
+        public int[,] grid = new int[20, 10];
 
         Block[,] currentBlock = new Block[4, 4];
 
         Block[] tempBlock = new Block[4];
+
+        public Block[,] gridBlock = new Block[20, 10];
 
         //public Point
 
@@ -36,9 +38,29 @@ namespace Tetris
             {
                 for (int j = 0; j < 10; j++)
                 {
+                    //Block block = new Block();
+                    //grid[i, j] = block;
+                    //tableLayoutPanel1.Controls.Add(block, j, i);
+
+                    grid[i, j] = 0;
                     Block block = new Block();
-                    grid[i, j] = block;
+                    gridBlock[i, j] = block;
                     tableLayoutPanel1.Controls.Add(block, j, i);
+                }
+            }
+
+            
+
+            UpdateGrid();
+        }
+
+        private void UpdateGrid()
+        {
+            foreach (int i in grid)
+            {
+                if (i != 0)
+                {
+                    
                 }
             }
         }
@@ -57,18 +79,19 @@ namespace Tetris
             {
                 for (int j = 0; j < block.GetLength(1); j++)
                 {
-                    OBlock oB = new OBlock();
+                    //OBlock oB = new OBlock();
                     //grid[i, j + spawnPoit].BlockColor = Color.Yellow;
                     //var temp = grid[i, j + spawnPoit];
                     //temp.BackColor = Color.Red;
 
                     if (block[i, j] == 1)
                     {
+                        grid[i, j+ spawnPoit] = block[i, j];
                         //var test = grid[block[i, j], block[i, j] + spawnPoit];
                         //test.BackColor = Color.Yellow;
-                        var temp = grid[i, j + spawnPoit];
+                        var temp = gridBlock[i, j + spawnPoit];
                         temp.BackColor = Color.Red;
-                        currentBlock[i, j] = grid[i, j+spawnPoit];
+                        //currentBlock[i, j] = grid[i, j+spawnPoit];
                     }
 
                 }
@@ -77,6 +100,7 @@ namespace Tetris
 
 
             Console.WriteLine("done");
+            DebugGrid();
 
         }
 
@@ -91,6 +115,18 @@ namespace Tetris
                         currentBlock[i, j].BackColor = Color.Aquamarine;
                     }
                 }
+            }
+        }
+
+        void DebugGrid()
+        {
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    Console.Write(grid[i,j]+ " ");
+                }
+                Console.WriteLine();
             }
         }
     }
